f6af3a9112fc3f326c4c8e5526ba3a045b921948
Parsa Dastjerdi <parsadastjerdi@gmail.com>
2019-02-14 21:38:48 -0600

    scaffolding for scripts

--- bin/election.py
+++ bin/election.py
@@ -1,1 +1,10 @@
+# use selenium to simulate web browser (need to download selenium or create a docker image)
+from selenium import webdriver
+import requests
+
+with requests.Session() as s:
+	driver = webdriver.Chrome()
+	driver.get("https://example.com")
+	button = driver.find_element_by_id('buttonID')
+	button.click()
 
--- bin/eligibility.py
+++ bin/eligibility.py
@@ -1,1 +1,35 @@
+import csv
 
+def parse_name(fullname):
+    '''
+        Overview:
+           Parses names in the format 'last, first middle' into a list with three elements that each contains a name
+        Input:
+            Name in the format 'last, first middle'
+        Returns:
+            Array with three elements, each element contains a name
+    '''
+    pass
+
+def parse_major(major):
+    '''
+        Overview:
+            Converts major from 
+        Input:
+            A string that contains the major in an acronym
+        Returns:
+            A string that contains the TBP official representation for that major.
+            If the major is unavailable, then the person is assigned "Safety Engineering".
+    '''
+    pass
+
+def generate_spreadsheet():
+    '''
+        Overview:
+            Generates a new, formatted spreadsheet from the original spreadsheet
+    '''
+    pass
+
+
+if __name__ == '__main__':
+    generate_spreadsheet()
